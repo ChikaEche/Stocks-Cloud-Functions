8fc2ab1cdc27c3a5c669fe359718a1b775444c0c
ChikaEche <chikaokafor94@gmail.com>
2021-06-04 17:58:55 +0100

    bug fix

--- index.js
+++ index.js
@@ -6,27 +6,22 @@ exports.stocksNews = async (req, res) => {
   const news = await fetch(`https://api.polygon.io/v2/reference/news?limit=5&order=descending&sort=published_utc&ticker=${ticker}&apiKey=${process.env.POLYGON_API_KEY}`);
 
   const { results } = await news.json();
-  const blocks = await results.map((result) => {
+  const blocks = results.map((result) => {
 
   const {title, published_utc, article_url, image_url, description } = result;
-  const slackStyle = [
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `*Title* \n ${title} \n *Description* \n ${description} \n *Time* \n ${published_utc} *Link* \n <${article_url}>`
-      },
-      "accessory": {
-        "type": "image",
-        "image_url": `${image_url}`,
-        "alt_text": "Image"
-      },
+  const slackTemplate = {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": `*Title* \n ${title} \n *Description* \n ${description} \n *Time* \n ${published_utc} \n *Link* \n <${article_url}>`
     },
-    {
-      "type": "divider"
-    }
-  ]
-  return slackStyle[0];
+    "accessory": {
+      "type": "image",
+      "image_url": `${image_url}`,
+      "alt_text": "Image"
+    },
+  }
+  return {...slackTemplate };
   });
 
   res.status(200).json({blocks});
